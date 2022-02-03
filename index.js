f7f428f9e8eca27b7060fc85d565321498bfddc8
Xavier Dunkley <xavierxv18@gmail.com>
2022-02-03 11:10:30 -0500

    commit tree b8ca86d0eca1a1c2cced98bb4c64cfb50922156b

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(activity="go to the office"){
+    return `This Monday, I will ${activity}.`;
+}
+mondayWork();
+
+function wrapAdjective(parameter="*"){
+    return function (adjective ="special"){
+        return `You are ${parameter}${adjective}${parameter}!`;
+    }
+}
+wrapAdjective("%")("a dedicated programmer");
